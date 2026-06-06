4ba7a8e95a55c20ad5d00fe5c511e0f70bcba988
honey4444s19 <honey4444s19@gmail.com>
2026-06-06 10:20:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const dog = document.getElementById("dog");
     const scoreText = document.getElementById("score");
-    const message = document.getElementById("message");
-    const ending = document.getElementById("ending");
+    const messageBox = document.getElementById("messageBox");
 
     let score = 0;
 
-    const texts = [
-        "🐶 Sorry!",
-        "🩹 Bandage for you!",
-        "❤️ Get well soon!",
-        "😊 You're amazing!"
-    ];
-
     function moveDog(){
         const x = Math.random() * (window.innerWidth - 100);
-        const y = Math.random() * (window.innerHeight - 150);
+        const y = Math.random() * (window.innerHeight - 100);
         dog.style.left = x + "px";
         dog.style.top = y + "px";
     }
@@ -28,16 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
         score++;
         scoreText.innerText = score;
 
-        message.innerText =
-            texts[Math.floor(Math.random() * texts.length)];
+        moveDog();
 
         if(score >= 15){
-            ending.style.display = "block";
             dog.style.display = "none";
-            return;
+            messageBox.style.display = "flex";
         }
 
-        moveDog();
     });
 
 });
